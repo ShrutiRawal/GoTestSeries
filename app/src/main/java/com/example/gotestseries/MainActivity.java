2e868109677738a0207cfb6963dbc06fb47bc71f
ShrutiRawal <shruti456rawal@gmail.com>
2020-03-17 12:02:54 +0530

    update

--- app/src/main/java/com/example/gotestseries/MainActivity.java
+++ app/src/main/java/com/example/gotestseries/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.gotestseries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.web);
         webView.loadUrl("https://gotestseries.com");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
     }
 }
