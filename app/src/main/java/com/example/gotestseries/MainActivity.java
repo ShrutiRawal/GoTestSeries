a8ed49e57de106dbb969edc4d3b0888c8c003f97
ShrutiRawal <shruti456rawal@gmail.com>
2020-03-17 11:50:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gotestseries/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gotestseries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.web);
+        webView.loadUrl("https://gotestseries.com");
+    }
+}
